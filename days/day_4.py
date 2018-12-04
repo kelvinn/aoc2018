2c1ecd71923746bc1033b033ed5d4561c83adba3
Kelvin Nicholson <kelvin@kelvinism.com>
2018-12-04 20:19:45 +1100

    Get ready for day4

---
+++ days/day_4.py
@@ -0,0 +1,9 @@
+
+def solve_task_1(data):
+
+    return 42
+
+
+def solve_task_2(data):
+
+    return 42
---
+++ tests/test_day_4.py
@@ -0,0 +1,23 @@
+import unittest
+import days.day_4
+
+
+class TestDay4(unittest.TestCase):
+
+    def setUp(self):
+        with open('tests/data/day_4.txt', newline='') as f:
+            self.data = f.read()
+
+    def test_task_1(self):
+
+        result_task_1 = days.day_4.solve_task_1(self.data)
+        self.assertEqual(result_task_1, 42)
+
+    def test_task_2(self):
+
+        result_task_2 = days.day_4.solve_task_2(self.data)
+        self.assertEqual(result_task_2, 42)
+
+
+if __name__ == '__main__':
+    unittest.main()
