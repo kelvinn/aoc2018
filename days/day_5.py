22411afca7ae1a39d4ee4345796fd502ebac18b7
Kelvin Nicholson <kelvin@kelvinism.com>
2018-12-06 20:02:22 +1100

    Day 5

--- days/day_5.py
+++ days/day_5.py
@@ -1,13 +1,52 @@
+import string
 
-def parse_line(line):
-    pass
+lower_upper = list(zip(list(string.ascii_lowercase), list(string.ascii_uppercase)))
+upper_lower = list(zip(list(string.ascii_uppercase), list(string.ascii_lowercase)))
+ALL_PAIRS = ['%s%s' % pair for pair in lower_upper + upper_lower]
+
+
+# First implementation. Super slow.
+def pop_pairs(data):
+
+    for i in range(0, len(data)):
+        if i < len(data) - 1 \
+                and data[i] != data[i+1] and (data[i].upper() == data[i+1] or data[i].lower() == data[i+1]):
+            data.pop(i+1)
+            data.pop(i)
+            return data, True
+    return data, False
+
+
+# Faster implementation
+def find_and_pop(data):
+
+    for pair in ALL_PAIRS:
+        if data.count(pair):
+            return data.replace(pair, ''), True
+    return data, False
 
 
 def solve_task_1(data):
 
-    return 42
+    cont = True
+    while cont:
+        data, cont = find_and_pop(data)
+    return len(data)
 
 
 def solve_task_2(data):
 
-    return 42
+    lu_list = list(zip(list(string.ascii_lowercase), list(string.ascii_uppercase)))
+
+    all_letters = []
+
+    for l, u in lu_list:
+        temp_data = data
+        temp_data = temp_data.replace(l, '').replace(u, '')
+        cont = True
+        while cont:
+            temp_data, cont = find_and_pop(temp_data)
+        print(l, u, len(temp_data))
+        all_letters.append(len(temp_data))
+
+    return min(all_letters)
--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -5,18 +5,18 @@ import days.day_5
 class TestDay5(unittest.TestCase):
 
     def setUp(self):
-        with open('tests/data/day_4.txt', newline='') as f:
+        with open('tests/data/day_5.txt', newline='') as f:
             self.data = f.read()
 
     def test_task_1(self):
 
         result_task_1 = days.day_5.solve_task_1(self.data)
-        self.assertEqual(result_task_1, 10)
+        self.assertEqual(10978, result_task_1)  # 10978
 
     def test_task_2(self):
 
         result_task_2 = days.day_5.solve_task_2(self.data)
-        self.assertEqual(result_task_2, 42)
+        self.assertEqual(result_task_2, 4840)
 
 
 if __name__ == '__main__':
