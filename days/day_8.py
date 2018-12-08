c5641876bcadabd3da5e9b7b52d25c0c68d310d9
Kelvin Nicholson <kelvin@kelvinism.com>
2018-12-08 21:47:16 +1100

    Prepare for day 8

---
+++ days/day_8.py
@@ -0,0 +1,16 @@
+
+
+def parse_line(line):
+    return True
+
+
+
+def solve_task_1(data):
+
+    return 42
+
+
+def solve_task_2(data):
+
+
+    return 42
---
+++ tests/test_day_8.py
@@ -0,0 +1,23 @@
+import unittest
+import days.day_8
+
+
+class TestDay8(unittest.TestCase):
+
+    def setUp(self):
+        with open('tests/data/day_8.txt', newline='') as f:
+            self.data = f.read()
+
+    def test_task_1(self):
+
+        result_task_1 = days.day_8.solve_task_1(self.data)
+        self.assertEqual(42, result_task_1)
+
+    def test_task_2(self):
+
+        result_task_2 = days.day_8.solve_task_2(self.data)
+        self.assertEqual(42, result_task_2)
+
+
+if __name__ == '__main__':
+    unittest.main()
