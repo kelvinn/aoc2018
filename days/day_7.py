509d75f7c6ab14269c80dc5d945f9ccb23d6cb6b
Kelvin Nicholson <kelvin@kelvinism.com>
2018-12-08 14:32:30 +1100

    Day 7

---
+++ days/day_7.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from collections import Counter
+import operator
+
+
+def parse_line(line):
+    return True
+
+
+def solve_task_1(data):
+
+    return int(data)
+
+
+def solve_task_2(data):
+
+    return int(data)
